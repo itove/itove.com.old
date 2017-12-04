60a53461dba17400a29e056ae3edd3374c183d90
Dot <dotcra@gmail.com>
2017-12-04 10:42:25 +0800

    nth

--- yq/i.js
+++ yq/i.js
@@ -1,26 +1,7 @@
-$("#caro_1").click(
+$(".host").click(
 	function(){
-		$('#caro_1 + div').toggleClass('vanish');
-		$('#caro_1 .fa-plus').toggleClass('fa-minus');
-	}
-);
-
-$("#caro_2").click(
-	function(){
-		$('#caro_2 + div').toggleClass('vanish');
-		$('#caro_2 .fa-plus').toggleClass('fa-minus');
-	}
-);
-
-$("#caro_3").click(
-	function(){
-		$('#caro_3 + div').toggleClass('vanish');
-		$('#caro_3 .fa-plus').toggleClass('fa-minus');
-	}
-);
-$("#caro_4").click(
-	function(){
-		$('#caro_4 + div').toggleClass('vanish');
-		$('#caro_4 .fa-plus').toggleClass('fa-minus');
+		var a = '#' + this.id;
+		$(a + ' + div').toggleClass('vanish');
+		$(a + ' .fa-plus').toggleClass('fa-minus');
 	}
 );
