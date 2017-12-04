d06a87a1ff2bbd283c5bcf2b4887658788b4a832
Dot <dotcra@gmail.com>
2017-12-04 09:25:10 +0800

    nth

---
+++ yq/i.js
@@ -0,0 +1,26 @@
+$("#caro_1").click(
+	function(){
+		$('#caro_1 + div').toggleClass('vanish');
+		$('#caro_1 .fa-plus').toggleClass('fa-minus');
+	}
+);
+
+$("#caro_2").click(
+	function(){
+		$('#caro_2 + div').toggleClass('vanish');
+		$('#caro_2 .fa-plus').toggleClass('fa-minus');
+	}
+);
+
+$("#caro_3").click(
+	function(){
+		$('#caro_3 + div').toggleClass('vanish');
+		$('#caro_3 .fa-plus').toggleClass('fa-minus');
+	}
+);
+$("#caro_4").click(
+	function(){
+		$('#caro_4 + div').toggleClass('vanish');
+		$('#caro_4 .fa-plus').toggleClass('fa-minus');
+	}
+);
