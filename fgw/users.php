31858c7e0ce04658ac652c4f867f6dbcf527f821
Dot <dotcra@gmail.com>
2018-02-04 02:06:04 +0800

    nth

--- fgw/users.php
+++ fgw/users.php
@@ -1,5 +1,5 @@
 <?php
-if(!empty($_POST)){
+if($_POST){
 	//var_dump($_POST);
 	foreach($_POST as $v){
 		if(empty($v)){
@@ -9,9 +9,27 @@ if(!empty($_POST)){
 	}
 	// if anyone in $_POST is empty, not do this
 	if($v){
-		$sql="insert into users (uname, passwd, oid, rid) values(" . '"' . $_POST['uname']  . '", "' . md5($_POST['passwd']) . '", "' . $_POST['oid'] . '", "' . $_POST['rid'] . '")';
-		//echo $sql;
-		(new Db)->query($sql);
+		// valiate uname
+		$newuser = $_POST['uname'];
+		$newpass = $_POST['passwd'];
+		$o = ['options' => ['regexp' => '/^[[:alpha:]]\w{3,10}$/']];
+		if(filter_var($newuser, FILTER_VALIDATE_REGEXP, $o)){
+			// we only allow lower case
+			$newuser = strtolower($newuser);
+			// if uname not duplicate
+			$sql = "select uname from users where uname='$newuser'";
+			if((new Db)->query($sql)==null){
+				$sql="insert into users (uname, passwd, oid, rid) values(" . '"' . $newuser  . '", "' . md5($_POST['passwd']) . '", "' . $_POST['oid'] . '", "' . $_POST['rid'] . '")';
+				//echo $sql;
+				(new Db)->query($sql);
+			}
+			else{
+				$alert = 2;
+			}
+		}
+		else{
+			$alert = 1;
+		}
 	}
 }
 
@@ -25,13 +43,27 @@ $sql="select users.uid,uname,organization.oname,role.rname from users join (orga
 $u_rows=(new Db)->query($sql);
 ?>
 		  <main>
+<?php if($alert): ?>
+		  <div class="alert alert-danger alert-dismissible fade show" role="alert">
+<?php if($alert == 1): ?>
+		  用户名由字母和数字组成，不能包含特殊字符，长度3至10位。第一位不能是数字！
+<?php endif ?>
+<?php if($alert == 2): ?>
+		  用户名<?= $newuser ?>已存在，试试别的！
+<?php endif ?>
+			  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				  <span aria-hidden="true">&times;</span>
+			  </button>
+		  </div>
+<?php endif ?>
+
 		  <div class="row mb-3">
 			  <form class="form-row col-sm" method="post">
 				  <div class="col-sm">
-					  <input type="text" class="form-control" name="uname" placeholder="用户名">
+					  <input type="text" class="form-control" name="uname" placeholder="用户名" required>
 				  </div>
 				  <div class="col-sm">
-					  <input type="password" class="form-control" name="passwd" placeholder="密 码">
+					  <input type="password" class="form-control" name="passwd" placeholder="密 码" required>
 				  </div>
 				  <div class="col-sm">
 					  <select class="form-control" name="oid">
