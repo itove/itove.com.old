9c012bb01335f5266ab5e095c088b1cad8423021
Dot <dotcra@gmail.com>
2018-02-09 10:37:15 +0800

    nth

--- fgw/home.php
+++ fgw/home.php
@@ -74,7 +74,8 @@ $uname=$_SESSION['uname'];
 			  </div>
 		  </div>
 	  </div>
-	  <div class="row marketing">
+<!--
+	  <div class="row">
 		  <div class="col-lg-6">
 			  <h4>Subheading</h4>
 			  <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
@@ -97,6 +98,7 @@ $uname=$_SESSION['uname'];
 			  <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
 		  </div>
 	  </div>
+-->
 	  </main>
 	</div>
 
