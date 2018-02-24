a9b192f2f7839582e1db12f19782aba72a7fb7e0
Dot <dotcra@gmail.com>
2018-02-24 18:17:37 +0800

    rm fgw/ from parent repo

--- fgw/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-function myautoload($class){
-	require $class . '.php';
-}
-spl_autoload_register('myautoload');
-
-// spl_autoload_register(function ($class){
-// 	require_once $class . '.php';
-// });
