ffd5e14f119a3042cbec7e3fca947eaa2bbbed57
Dot <dotcra@gmail.com>
2018-03-29 11:23:44 +0800

    nth

--- demo/gk/js/dot.js
+++ demo/gk/js/dot.js
@@ -2,7 +2,9 @@ var bars = document.getElementById('bars');
 if (bars) bars.addEventListener('click', toggleSidebar);
 function toggleSidebar(){
 	var navul = document.getElementById('navul');
-	var sidebar = document.getElementById('sidebar');
+	//var sidebar = document.getElementById('sidebar');
 	navul.classList.toggle('d-none');
+	bars.classList.toggle('text-light');
+	bars.classList.toggle('text-warning');
 }
 
