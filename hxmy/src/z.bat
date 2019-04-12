ad0f210b0965145e3034b94a2377a0c425753acb
Arc Zee <z@arcz.ee>
2019-04-12 16:39:53 +0800

    nth

--- hxmy/src/z.bat
+++ hxmy/src/z.bat
@@ -3,8 +3,7 @@ cd z
 
 for /f %%i in ('osql -S HXMYSRV -d dlhotel -U sa -P -h-1 -n -i a.sql') do set num=%%i
 
-rem set /p num=<r.txt
+curl --cacert cacert.pem -d "num=%num%&key=fuck" https://itove.com/hxmy/q/
 
 echo %num% > t.txt
 
-curl --cacert cacert.pem -d "num=%num%&key=fuck" https://itove.com/hxmy/q/
