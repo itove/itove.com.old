a7c84564b22c225c6d158c8bceb790f66dc6be4e
Arc Zee <z@arcz.ee>
2019-04-12 08:58:29 +0800

    add hxmy

---
+++ hxmy/q/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * vim:ft=php
+ * @author Dotcra <dotcra@gmail.com>
+ * @version
+ * @todo
+ */
+require '../vendor/autoload.php';
+$num = $_POST['num'];
+var_dump($num);
+
+// write to redis;
+
+// $redis = new Redis();
+// $redis->connect('127.0.0.1', 6379);
+// $redis->time();
+// var_dump($redis);
+ 
+Predis\Autoloader::register();
+
+$client = new Predis\Client();
+echo 'yes';
+$client->set('num', '9999'); // php index.php ok; curl not?
+echo 'fuck';
---
+++ hxmy/src/z.bat
@@ -0,0 +1,10 @@
+d:
+cd z
+
+for /f %%i in ('osql -S HXMYSRV -d dlhotel -U sa -P -h-1 -n -i a.sql') do set num=%%i
+
+rem set /p num=<r.txt
+
+echo %num% > t.txt
+
+curl --cacert cacert.pem -d "num=%num%" https://itove.com/hxmy/q/
