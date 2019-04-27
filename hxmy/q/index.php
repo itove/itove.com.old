e6f70eb369adc12aa4a078882f88c91f0bbf0b2e
Arc Zee <z@arcz.ee>
2019-04-27 11:16:47 +0800

    nth

--- hxmy/q/index.php
+++ hxmy/q/index.php
@@ -11,17 +11,18 @@ require '../vendor/autoload.php';
 $client = new Predis\Client();
 
 if (isset($_POST['oc']) && isset($_POST['rsv']) && isset($_POST['key']) && $_POST['key'] == 'fuck'){
-	$oc = $_POST['oc'];
-	$rsv = $_POST['rsv'];
-	$client->set('oc', $oc);
-	$client->set('rsv', $rsv);
-	$client->set('sec', date('U'));
+    $oc = $_POST['oc'];
+    $rsv = $_POST['rsv'];
+    $client->set('oc', $oc);
+    $client->set('rsv', $rsv);
+    $client->set('sec', date('U'));
 }
 else {
-	$a = [
-		'oc' => $client->get('oc'),
-		'rsv' => $client->get('rsv'),
-		'sec' => $client->get('sec')
-	];
-	echo json_encode($a, JSON_UNESCAPED_UNICODE);
+    $a = [
+        'oc' => $client->get('oc'),
+        'rsv' => $client->get('rsv'),
+        'sec' => $client->get('sec')
+    ];
+    header('Content-Type: application/json');
+    echo json_encode($a, JSON_PRETTY_PRINT);
 }
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * vim:ft=php
+ * @author Dotcra <dotcra@gmail.com>
+ * @version
+ * @todo
+ */
+
+
+echo file_get_contents('php://input');
